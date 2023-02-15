a1959fe933570b966c3da522f417cadb8d380e1c
Shrestha Datta <52985769+Shrestha30@users.noreply.github.com>
2023-02-15 21:13:01 +0600

    completed clinical data prediction backend

--- flask-server/app.py
+++ flask-server/app.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
 import os
 from config import ApplicationConfig
-from models import db, User, Image
+from models import db, User, Image, ClinicalData
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
@@ -103,7 +103,7 @@ def upload():
     
     file.save(filepath)
     
-    prediction='1'
+    prediction='1'#assign prediction function here
     
     if request.form.get('save')=='yes':
         imgentry = Image.query.filter(Image.uid==uid and Image.date==date and Image.eye==eye).first()
@@ -113,10 +113,60 @@ def upload():
             db.session.commit()
         else:
             imgentry.prediction = prediction
-            db.session.commit() 
+            db.session.commit()
     
-    print(file)
-    return {"rediction": prediction}
+    # print(file)
+    if prediction=='1':
+        prediction='glaucoma'
+    else: prediction='no'
+    
+    return {"prediction": prediction}
+
+@app.route('/clinicalpredict',methods=['POST'])
+def clinicalpredict():
+    uid = request.json['uid']
+    date = request.json['date']
+    eye = request.json['eye']
+    save = request.json['save']
+    dioptre1 = request.json['dioptre1']
+    dioptre2 = request.json['dioptre2']
+    astigmatism = request.json['astigmatism']
+    phakic = request.json['phakic']
+    gender = request.json['gender']
+    age = request.json['age']
+    pneumatic = request.json['pneumatic']
+    perkins = request.json['perkins']
+    pachymetry = request.json['pachymetry']
+    axiallength = request.json['axiallength']
+    vfmd = request.json['vfmd']
+    
+    prediction='1'#assign prediction function here
+    
+    if save=='1':
+        clinicalDataEntry = ClinicalData.query.filter(ClinicalData.uid==uid and ClinicalData.date==date and ClinicalData.eye==eye).first()
+        if clinicalDataEntry is None:
+            clinicalDataEntry = ClinicalData(uid=uid,date=date,eye=eye,prediction=prediction,dioptre1=dioptre1,dioptre2=dioptre2,gender=gender,age=age,astigmatism=astigmatism,phakic=phakic,pneumatic=pneumatic,perkins=perkins,pachymetry=pachymetry,axiallength=axiallength,vfmd=vfmd)
+            db.session.add(clinicalDataEntry)
+            db.session.commit()
+        else:
+            clinicalDataEntry.prediction = prediction
+            clinicalDataEntry.dioptre1 = dioptre1
+            clinicalDataEntry.dioptre2 = dioptre2
+            clinicalDataEntry.gender = gender
+            clinicalDataEntry.age = age
+            clinicalDataEntry.astigmatism = astigmatism
+            clinicalDataEntry.phakic = phakic
+            clinicalDataEntry.pneumatic = pneumatic
+            clinicalDataEntry.perkins = perkins
+            clinicalDataEntry.pachymetry = pachymetry
+            clinicalDataEntry.axiallength = axiallength
+            clinicalDataEntry.vfmd = vfmd
+            db.session.commit()
+    if prediction=='1':
+        prediction='glaucoma'
+    else: prediction='no'
+    
+    return {"prediction": prediction}
 
 
 
